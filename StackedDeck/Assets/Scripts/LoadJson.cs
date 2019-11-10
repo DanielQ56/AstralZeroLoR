eb352dc0eb1ab22f133e5f6e600f03cecf2618d7
Daniel Qi <drqi@gmail.com>
2019-11-10 02:07:20 -0800

    started working on reading json file

---
+++ StackedDeck/Assets/Scripts/LoadJson.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class LoadJson
+{
+    public static string LoadResourceTextFile()
+    {
+        return "hello";
+    }
+}
